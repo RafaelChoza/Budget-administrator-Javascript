263ade14ba963bf140d1d3664c25897660a429a1
Rafael <166154431+RafaelChoza@users.noreply.github.com>
2024-09-26 07:34:47 -0600

    Update main.js
    remove budget update function

--- js/main.js
+++ js/main.js
@@ -42,18 +42,8 @@ submitButton.addEventListener('click', function() {
     addExpenseIcon.classList.remove('hide')
 
     window.budgetValue = budgetValue.toFixed(2);
-
-    location.reload(); //Se recarga la pagina
     
 });
-
-const editBudgeButton = document.querySelector('.edit__budgetButton')
-
-editBudgeButton.addEventListener('click', () => {
-    budgetInput.classList.remove('hide')
-})
-
-
 
 const addExpenseIcon = document.querySelector('.add__expenseIcon')
 const backModalSection = document.querySelector('.div_backModal')
